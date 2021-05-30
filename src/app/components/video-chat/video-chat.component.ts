fab9e5c4d54f1a297cd227bce2f5273d4902825d
Moy <moy_rub@hotmail.com>
2021-05-30 17:25:11 -0500

    button reunion

--- src/app/components/video-chat/video-chat.component.ts
+++ src/app/components/video-chat/video-chat.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-video-chat',
   templateUrl: './video-chat.component.html',
-  styleUrls: ['./video-chat.component.css']
+  styleUrls: ['./video-chat.component.css'],
 })
 export class VideoChatComponent implements OnInit {
+  message = '';
+  goToRoom = () => {
+    this.message = 'Entrando...';
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
