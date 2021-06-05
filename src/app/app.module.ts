e14c59f41545708a8bcc3b1e5a0b2aaeb64f1ba6
Moy <moy_rub@hotmail.com>
2021-06-05 13:52:46 -0500

    Navbar y Footer AGREGADOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { VideoChatComponent } from './components/video-chat/video-chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoChatComponent
+    VideoChatComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
